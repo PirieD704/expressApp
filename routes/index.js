d2ed7c193ecf7e159ce0e745905818fb1fb5316f
PirieD704 <dp75975@gmail.com>
2016-09-01 19:58:44 -0400

    express  app, intro to jade(pug), and ejs

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+   title: 'About Page',
+   students: [
+   		'JT',
+   		'Danny',
+   		'Dan',
+   		'Alex'
+   ]
+  });
+});
+
+/* GET home page. */
+router.get('/weather', function(req, res, next) {
+	var apikey = 'e312dbeb8840e51f92334498a261ca1d';
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&APPID="+apikey;
+    request.get(weatherUrl, function(error, response, body){
+    	body = JSON.parse(body);
+    	res.render('weather', {weatherObject: body, varType: typeof(body)});
+    })
+});
+
+router.get('/movies', function(req, res, next) {
+	var apiBaseUrl = 'http://api.themoviedb.org/3/';  
+	var imageBaseUrl = 'http://image.tmdb.org/t/p/';
+	var apiKey = '?api_key=fec8b5ab27b292a68294261bb21b04a5';
+	var npUrl  = apiBaseUrl + 'movie/now_playing' + apiKey;
+    request.get(npUrl, function(error, response, body){
+    	body = JSON.parse(body);
+    	res.render('movies', {imageBaseUrl, moviesObject: body, varType: typeof(body)});
+    	console.log(body);
+    })
+});
+
+
+module.exports = router;
